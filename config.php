6f1a7aff0a882ac386b43f1bcfd14ec21cd0769f
Everton da Rosa <everton3x@gmail.com>
2022-06-27 10:45:14 -0300

    config update

--- config.php
+++ config.php
@@ -7,15 +7,17 @@ use PTK\FS\Path;
  * 
  */
 
-$mes = 1;
+$mes = 5;
 $ano = 2022;
 
 
 $mes = str_pad($mes, 2, '0', STR_PAD_LEFT);
 $inputDir = [
-    (new Path('C:/Users/Everton/OneDrive/Prefeitura', $ano, 'PAD', "$ano-$mes", 'pm', "MES$mes"))->getPath(),
-    (new Path('C:/Users/Everton/OneDrive/Prefeitura', $ano, 'PAD', "$ano-$mes", 'cm', "MES$mes"))->getPath(),
+//    (new Path('C:/Users/Everton/OneDrive/Prefeitura', $ano, 'PAD', "$ano-$mes", 'pm', "MES$mes"))->getPath(),
+//    (new Path('C:/Users/Everton/OneDrive/Prefeitura', $ano, 'PAD', "$ano-$mes", 'cm', "MES$mes"))->getPath(),
+	(new Path('Z:/Abase/ARQUIVOSPAD', $ano, "MES$mes"))->getPath(),
+	(new Path('Z:/Abase/ARQUIVOSPAD', $ano, 'CAMARA', "MES$mes"))->getPath(),
 ];
 
-$outputSQLite = (new Path('C:/Users/Everton/OneDrive/Prefeitura/PAD', "$ano-$mes.sqlite"))->getPath();
+//$outputSQLite = (new Path('C:/Users/Everton/OneDrive/Prefeitura/PAD', "$ano-$mes.sqlite"))->getPath();
 $outputCSV = (new Path('C:/Users/Everton/OneDrive/Prefeitura/PAD', "$ano-$mes"))->getPath();
