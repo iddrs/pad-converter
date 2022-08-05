717d376bdf4b18a4335c42f2048dd91028c5ee74
Everton da Rosa <everton3x@gmail.com>
2022-08-05 15:32:28 -0300

    Adicionado digitação de mês e ano em vez de abrir o arquivo de configuração

--- config.php
+++ config.php
@@ -1,22 +1,45 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
 use PTK\FS\Path;
+use PTK\Console\Form\Field\NumberField;
 
 /**
  * Arquivo de configuração coms os parâmetros de conversão
  * 
  */
 
-$mes = 12;
-$ano = 2018;
+//$mes = 6;
+//$ano = 2022;
 
+$getMes = new NumberField('mes', 'Mês:');
+$getMes->setMin(1);
+$getMes->setMax(12);
+//$getMes->onlyInt(true);
+$getMes->required(true);
+$getMes->showDefaultInLabel(false);
+$getMes->showFormatInLabel(false);
+$getMes->showMinMaxInLabel(false);
 
-$mes = str_pad($mes, 2, '0', STR_PAD_LEFT);
+$getAno = new NumberField('ano', 'Ano:');
+//$getAno->onlyInt(true);
+$getAno->required(true);
+$getAno->showDefaultInLabel(false);
+$getAno->showFormatInLabel(false);
+$getAno->showMinMaxInLabel(false);
+
+$getMes->ask();
+$getAno->ask();
+
+$mes = str_pad($getMes->answer(), 2, '0', STR_PAD_LEFT);
+$ano = $getAno->answer();
+
 $inputDir = [
-//    (new Path('C:/Users/Everton/OneDrive/Prefeitura', $ano, 'PAD', "$ano-$mes", 'pm', "MES$mes"))->getPath(),
-//    (new Path('C:/Users/Everton/OneDrive/Prefeitura', $ano, 'PAD', "$ano-$mes", 'cm', "MES$mes"))->getPath(),
-	(new Path('Z:/Abase/ARQUIVOSPAD', $ano, "MES$mes"))->getPath(),
-	(new Path('Z:/Abase/ARQUIVOSPAD', $ano, 'CAMARA', "MES$mes"))->getPath(),
+    (new Path('C:/Users/Everton/OneDrive/Prefeitura', $ano, 'PAD', "$ano-$mes", 'pm', "MES$mes"))->getPath(),
+    (new Path('C:/Users/Everton/OneDrive/Prefeitura', $ano, 'PAD', "$ano-$mes", 'cm', "MES$mes"))->getPath(),
+//	(new Path('Z:/Abase/ARQUIVOSPAD', $ano, "MES$mes"))->getPath(),
+//	(new Path('Z:/Abase/ARQUIVOSPAD', $ano, 'CAMARA', "MES$mes"))->getPath(),
 ];
 
 //$outputSQLite = (new Path('C:/Users/Everton/OneDrive/Prefeitura/PAD', "$ano-$mes.sqlite"))->getPath();
--- convert.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-notepad.exe config.php
-
-php toCSV.php
-rem php toSQLite.php
-
