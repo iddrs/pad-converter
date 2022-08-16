0f1d5598bdbbe9641ae36caec8ef4e848138d103
Everton da Rosa <everton3x@gmail.com>
2022-08-16 14:53:34 -0300

    Adicionado campos de tipo de receita e receita base em arquivos de receita alternativos

--- src/Assembler/BalRecAltAssembler.php
+++ src/Assembler/BalRecAltAssembler.php
@@ -21,6 +21,26 @@ class BalRecAltAssembler implements AssemblerInterface {
     public function assemble(): DataFrame {
         $data = $this->balRec->getAsArray();
         foreach($data as $index => $item){
+            if($data[$index]['codigo_receita'][0] == 4){
+                switch($data[$index]['codigo_receita'][2]){
+                    case 1:
+                    case 2:
+                        $data[$index]['tipo_receita'] = 'normal';
+                        $data[$index]['base_receita'] = substr($item['codigo_receita'], 2);
+                        break;
+                    case 7:
+                        $data[$index]['tipo_receita'] = 'intra';
+                        $data[$index]['base_receita'] = "1".substr($item['codigo_receita'], 3);
+                        break;
+                    case 8:
+                        $data[$index]['tipo_receita'] = 'intra';
+                        $data[$index]['base_receita'] = "2".substr($item['codigo_receita'], 3);
+                        break;
+                }
+            }else{
+                $data[$index]['tipo_receita'] = 'dedutora';
+                $data[$index]['base_receita'] = substr($item['codigo_receita'], 2);
+            }
             $data[$index]['codigo_receita'] = substr($item['codigo_receita'], 2);
         }
         return new DataFrame(new ArrayReader($data));
